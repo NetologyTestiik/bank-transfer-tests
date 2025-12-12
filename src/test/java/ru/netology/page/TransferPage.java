65b141efb2d6e1ba3a7245d33d5aa456366c5509
NetologyTestiik <veronikakaravanova60@gmail.com>
2025-12-12 23:34:23 +0500

    Update TransferPage.java

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -1,26 +1,54 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import static com.codeborne.selenide.Condition.visible;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
-    
-    public TransferPage() {
-        // Проверяем, что форма перевода открыта
-        $("[data-test-id='amount']").shouldBe(visible);
-    }
-    
-    // Методы для работы с формой перевода
+    private SelenideElement amountField = $("[data-test-id='amount'] input");
+    private SelenideElement fromField = $("[data-test-id='from'] input");
+    private SelenideElement transferButton = $("[data-test-id='action-transfer']");
+    private SelenideElement cancelButton = $("[data-test-id='action-cancel']");
+    private SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
+    private SelenideElement amountError = $("[data-test-id='amount'] .input__sub");
+    private SelenideElement fromError = $("[data-test-id='from'] .input__sub");
+
     public SelenideElement getAmountField() {
-        return $("[data-test-id='amount'] input");
+        return amountField;
     }
-    
+
     public SelenideElement getFromField() {
-        return $("[data-test-id='from'] input");
+        return fromField;
     }
-    
+
     public SelenideElement getTransferButton() {
-        return $("[data-test-id='action-transfer']");
+        return transferButton;
+    }
+
+    public SelenideElement getCancelButton() {
+        return cancelButton;
+    }
+
+    public SelenideElement getErrorNotification() {
+        return errorNotification;
+    }
+
+    public SelenideElement getAmountError() {
+        return amountError;
+    }
+
+    public SelenideElement getFromError() {
+        return fromError;
+    }
+
+    public void makeTransfer(String amount, String fromCard) {
+        amountField.setValue(amount);
+        fromField.setValue(fromCard);
+        transferButton.click();
+    }
+
+    public void clickCancel() {
+        cancelButton.click();
     }
 }
