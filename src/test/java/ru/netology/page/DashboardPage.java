7e2b3e13b6c688f2e47f694c819491e090383ad4
NetologyTestiik <veronikakaravanova60@gmail.com>
2025-12-13 00:20:17 +0500

    Update DashboardPage.java

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -1,43 +1,47 @@
 package ru.netology.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import ru.netology.page.TransferPage;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     
+    // КОНСТРУКТОР - без проверок
     public DashboardPage() {
-        // Проверяем, что мы на странице Dashboard
-        $("[data-test-id='dashboard']").shouldBe(Condition.visible);
+        // НИКАКИХ shouldBe() в конструкторе
     }
 
-    // Проверка видимости карт с четырьмя цифрами
-    public void cardsShouldBeVisible() {
-        $("body").shouldHave(Condition.text("**** 0001"));
-        $("body").shouldHave(Condition.text("**** 0002"));
+    // МЕТОДЫ ВОЗВРАЩАЮТ ЭЛЕМЕНТЫ (без проверок)
+    public SelenideElement getFirstCardElement() {
+        return $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
     }
-
-    // Нажатие на кнопку "Пополнить" для определенной карты
+    
+    public SelenideElement getSecondCardElement() {
+        return $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
+    }
+    
+    public SelenideElement getDepositButtonForFirstCard() {
+        return $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit']");
+    }
+    
+    public SelenideElement getDepositButtonForSecondCard() {
+        return $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] [data-test-id='action-deposit']");
+    }
+    
+    // МЕТОДЫ ДЕЙСТВИЙ - только клики
     public TransferPage clickDepositOnFirstCard() {
-        $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit']")
-            .shouldBe(Condition.visible)
-            .shouldHave(Condition.text("Пополнить"))
-            .click();
+        getDepositButtonForFirstCard().click();
         return new TransferPage();
     }
     
     public TransferPage clickDepositOnSecondCard() {
-        $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] [data-test-id='action-deposit']")
-            .shouldBe(Condition.visible)
-            .shouldHave(Condition.text("Пополнить"))
-            .click();
+        getDepositButtonForSecondCard().click();
         return new TransferPage();
     }
 
-    // Метод для получения баланса (если нужно в будущем)
+    // МЕТОД ДЛЯ ПОЛУЧЕНИЯ БАЛАНСА
     public int getCardBalance(String lastFourDigits) {
         ElementsCollection cards = $$(".list__item");
         for (SelenideElement card : cards) {
@@ -48,58 +52,27 @@ public class DashboardPage {
         }
         return 0;
     }
-    
 
-private int extractBalance(String text) {
-    // Ищем баланс в разных форматах
-    String[] patterns = {"баланс: ", "Баланс: ", "balance: "};
-    
-    for (String pattern : patterns) {
-        int startIndex = text.indexOf(pattern);
-        if (startIndex != -1) {
-            startIndex += pattern.length();
-            // Ищем конец числа (руб., ₽, р., или конец строки)
-            int endIndex = text.length();
-            String[] endMarkers = {" р.", " руб.", " ₽", "\n", " "};
-            for (String marker : endMarkers) {
-                int markerIndex = text.indexOf(marker, startIndex);
-                if (markerIndex != -1 && markerIndex < endIndex) {
-                    endIndex = markerIndex;
-                }
-            }
-            
-            String balanceStr = text.substring(startIndex, endIndex)
-                .replaceAll("\\s+", "")
-                .replaceAll("[^0-9]", "");
-            
-            try {
-                return Integer.parseInt(balanceStr);
-            } catch (NumberFormatException e) {
-                return 0;
-            }
+    private int extractBalance(String text) {
+        String balanceStart = "баланс: ";
+        String balanceFinish = " р.";
+        
+        int startIndex = text.indexOf(balanceStart);
+        if (startIndex == -1) return 0;
+        
+        startIndex += balanceStart.length();
+        int endIndex = text.indexOf(balanceFinish, startIndex);
+        
+        if (endIndex == -1) return 0;
+        
+        String balanceStr = text.substring(startIndex, endIndex)
+            .replaceAll("\\s+", "")
+            .replaceAll("[^0-9]", "");
+        
+        try {
+            return Integer.parseInt(balanceStr);
+        } catch (NumberFormatException e) {
+            return 0;
         }
-    }
-    return 0;
-}
-    
-    // Дополнительные методы для работы с картами
-    public SelenideElement getFirstCardElement() {
-        return $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']")
-            .shouldBe(Condition.visible, 15000);
-    }
-    
-    public SelenideElement getSecondCardElement() {
-        return $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']")
-            .shouldBe(Condition.visible, 15000);
-    }
-    
-    public SelenideElement getDepositButtonForFirstCard() {
-        return $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] [data-test-id='action-deposit']")
-            .shouldBe(Condition.visible, 15000);
-    }
-    
-    public SelenideElement getDepositButtonForSecondCard() {
-        return $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] [data-test-id='action-deposit']")
-            .shouldBe(Condition.visible, 15000);
     }
 }
